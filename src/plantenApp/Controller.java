2a2b3b40880285262d723be4140fdbd2076ef9bc
bradley velghe <bradley.velghe@student.vives.be>
2020-06-02 17:23:32 +0200

    Merging met fields Ilyas

--- src/plantenApp/Controller.java
+++ src/plantenApp/Controller.java
@@ -1,7 +1,4 @@
 package plantenApp;
-
-
-
 import plantenApp.java.dao.Database;
 import plantenApp.java.dao.InfoTablesDAO;
 import plantenApp.java.model.InfoTables;
@@ -16,181 +13,164 @@ import javafx.stage.Stage;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-
-
 public class Controller {
-    public ComboBox<String> cboType;
-    public ComboBox cboFamilie;
-    public TextField txtNaam;
-    public Spinner nudMinBloeiHoogte;
-    public Spinner nudMinBladHoogte;
-    public Spinner nudMaxBloeiHoogte;
-    public Spinner nudMaxBladHoogte;
-    public ComboBox cboHabitat;
-    public CheckBox chkZand;
-    public CheckBox chkLeem;
-    public CheckBox chkKlei;
-    public Slider slrBezonning;
-    public Slider slrVochtbehoefte;
-    public Button btnZoek;
-    public ComboBox<String> cboBladgrootte;
-    public Button btnminimize;
-    public Button btnExit;
-    public Button btnMaximize;
-    public ComboBox<String> cboRatio;
-    public ComboBox<String> cboSpruitFenologie;
-    public ImageView imgAar;
-    public ImageView imgBredePluim;
-    public ImageView imgEtage;
-    public ImageView imgKnop;
-    public ImageView imgScherm;
-    public ImageView imgSmallePluim;
-    public ImageView imgMargriet;
-    public ImageView imgPlatScherm;
-    public ComboBox<String> cboBladvorm;
-    public RadioButton rdbBloeiwijze1;
-    public RadioButton rdbBloeiwijze2;
-    public RadioButton rdbBloeiwijze3;
-    public RadioButton rdbBloeiwijze4;
-    public RadioButton rdbBloeiwijze5;
-    public RadioButton rdbBloeiwijze6;
-    public RadioButton rdbBloeiwijze7;
-    public RadioButton rdbBloeiwijze8;
-    public CheckBox chkHabitus1;
-    public CheckBox chkHabitus4;
-    public CheckBox chkHabitus7;
-    public CheckBox chkHabitus10;
-    public CheckBox chkHabitus13;
-    public CheckBox chkHabitus2;
-    public CheckBox chkHabitus5;
-    public CheckBox chkHabitus8;
-    public CheckBox chkHabitus11;
-    public CheckBox chkHabitus14;
-    public CheckBox chkHabitus3;
-    public CheckBox chkHabitus6;
-    public CheckBox chkHabitus9;
-    public CheckBox chkHabitus12;
-    public CheckBox chkHabitus15;
-    public Spinner cboFrequentie;
-    public ComboBox<String> cboMaand;
-    public ComboBox cboBehandeling;
-    public CheckBox chkBezonning;
-    public CheckBox chkVochtbehoefte;
-    public ListView lsvUitvoer;
-    public ComboBox<String> cboReactie;
-    public Slider slrOntwikkelingssnelheid;
-    public CheckBox chkLevensvorm2;
-    public CheckBox chkLevensvorm3;
-    public CheckBox chkLevensvorm4;
-    public CheckBox chkLevensvorm5;
-    public CheckBox chkLevensvorm6;
-    public CheckBox chkLevensvorm7;
-    public CheckBox chkLevensvorm8;
-    public CheckBox chkLevensvorm9;
-    public CheckBox chkSocI;
-    public CheckBox chkSocII;
-    public CheckBox chkSocIII;
-    public CheckBox chkSocIV;
-    public CheckBox chkSocV;
-    public CheckBox chkPollenwaarde;
-    public CheckBox chkNectarwaarde;
-    public Slider slrPollenwaarde;
-    public Slider slrNectarwaarde;
-    public CheckBox chkOntwikkelingsnelheid;
-    public Slider slrVoedingsbehoefte;
+    public TextField txtSearch;
+    public Spinner nudMinBladhoogte;
+    public Spinner nudMaxBladhoogte;
+    public Spinner nudMinBloeihoogte;
+    public Spinner nudBloeiHoogte;
+    public Slider sldBezonning;
+    public Slider sldVochtbehoefte;
+    public CheckBox chkGrondsoort_Z;
+    public CheckBox chkGrondsoort_L;
+    public CheckBox chkGrondsoort_K;
     public CheckBox chkVoedingsbehoefte;
-    public CheckBox chkLevensvorm1;
-    private boolean maximized=false;
+    public CheckBox chkReactieAntagonistischeOmg;
+    public CheckBox chkOntwikkelingssnelheid;
+    public CheckBox chkLevensduur_concurrentiekracht;
+    public CheckBox chkSociabiliteit;
+    public CheckBox chkStrategie;
+    public CheckBox chkNectarwaarde;
+    public CheckBox chkPollenwaarde;
+    public CheckBox chkBijvriendelijk;
+    public CheckBox chkVlindervriendelijk;
+    public CheckBox chkEetbaar;
+    public CheckBox chkKruidgebruik;
+    public CheckBox chkGeurend;
+    public CheckBox chkVorstgevoelig;
+    public CheckBox chkBladvorm;
+    public CheckBox chkRatio_bloei_blad;
+    public CheckBox chkSpruitfenologie;
+    public CheckBox chkBehandeling;
+    public CheckBox chkMaand;
+    public CheckBox chkPerXJaar;
+    public Slider sldVoedingsbehoefte;
+    public CheckBox chkSociabiliteit_1;
+    public CheckBox chkSociabiliteit_2;
+    public CheckBox chkSociabiliteit_3;
+    public CheckBox chkSociabiliteit_4;
+    public CheckBox chkSociabiliteit_5;
+    public RadioButton rdbStrategie_Onbekend;
+    public RadioButton rdbStrategie_C;
+    public RadioButton rdbStrategie_CR;
+    public RadioButton rdbStrategie_CRS;
+    public RadioButton rdbStrategie_CS;
+    public RadioButton rdbStrategie_R;
+    public RadioButton rdbStrategie_RS;
+    public RadioButton rdbStrategie_S;
+    public Slider sldNectarwaarde;
+    public Slider sldPollenwaarde;
+    public RadioButton rdbBijvriendelijk_Ja;
+    public RadioButton rdbBijvriendelijk_Nee;
+    public RadioButton rdbBijvriendelijk_Onbekend;
+    public RadioButton rdbVlindervriendelijk_Ja;
+    public RadioButton rdbVlindervriendelijk_Nee;
+    public RadioButton rdbVlindervriendelijk_Onbekend;
+    public RadioButton rdbEetbaar_Ja;
+    public RadioButton rdbEetbaar_Nee;
+    public RadioButton rdbEetbaar_Onbekend;
+    public RadioButton rdbKruidgebruik_Ja;
+    public RadioButton rdbKruidgebruik_Nee;
+    public RadioButton rdbKruidgebruik_Onbekend;
+    public RadioButton rdbGeurend_Ja;
+    public RadioButton rdbGeurend_Nee;
+    public RadioButton rdbGeurend_Onbekend;
+    public RadioButton rdbVorstgevoelig_Ja;
+    public RadioButton rdbVorstgevoelig_Nee;
+    public RadioButton rdbVorstgevoelig_Onbekend;
+    public Spinner nudPerXJaar;
+    public CheckBox chkMaxBladhoogte;
+    public Spinner nudMaxBladhoogte_Jan;
+    public Spinner nudMaxBladhoogte_Feb;
+    public Spinner nudMaxBladhoogte_Maa;
+    public Spinner nudMaxBladhoogte_Apr;
+    public Spinner nudMaxBladhoogte_Mei;
+    public Spinner nudMaxBladhoogte_Jun;
+    public Spinner nudMaxBladhoogte_Jul;
+    public Spinner nudMaxBladhoogte_Aug;
+    public Spinner nudMaxBladhoogte_Sept;
+    public Spinner nudMaxBladhoogte_Okt;
+    public Spinner nudMaxBladhoogte_Nov;
+    public Spinner nudMaxBladhoogte_Dec;
+    public CheckBox chkBladkleur;
+    public CheckBox chkMinBloeihoogte;
+    public Spinner nudMinBloeihoogte_Jan;
+    public Spinner nudMinBloeihoogte_Feb;
+    public Spinner nudMinBloeihoogte_Maa;
+    public Spinner nudMinBloeihoogte_Apr;
+    public Spinner nudMinBloeihoogte_Mei;
+    public Spinner nudMinBloeihoogte_Jun;
+    public Spinner nudMinBloeihoogte_Jul;
+    public Spinner nudMinBloeihoogte_Aug;
+    public Spinner nudMinBloeihoogte_Sept;
+    public Spinner nudMinBloeihoogte_Okt;
+    public Spinner nudMinBloeihoogte_Nov;
+    public Spinner nudMinBloeihoogte_Dec;
+    public CheckBox chkMaxBloeihoogte;
+    public Spinner nudMaxBloeihoogte_Jan;
+    public Spinner nudMaxBloeihoogte_Feb;
+    public Spinner nudMaxBloeihoogte_Maa;
+    public Spinner nudMaxBloeihoogte_Apr;
+    public Spinner nudMaxBloeihoogte_Mei;
+    public Spinner nudMaxBloeihoogte_Jun;
+    public Spinner nudMaxBloeihoogte_Jul;
+    public Spinner nudMaxBloeihoogte_Aug;
+    public Spinner nudMaxBloeihoogte_Sept;
+    public Spinner nudMaxBloeihoogte_Okt;
+    public Spinner nudMaxBloeihoogte_Nov;
+    public Spinner nudMaxBloeihoogte_Dec;
+    public CheckBox chkBloeikleur;
+    public CheckBox chkHabitus;
+    public RadioButton rdbHabitus_1;
+    public RadioButton rdbHabitus_2;
+    public RadioButton rdbHabitus_3;
+    public RadioButton rdbHabitus_4;
+    public RadioButton rdbHabitus_5;
+    public RadioButton rdbHabitus_6;
+    public RadioButton rdbHabitus_7;
+    public RadioButton rdbHabitus_8;
+    public RadioButton rdbHabitus_9;
+    public RadioButton rdbHabitus_10;
+    public RadioButton rdbHabitus_11;
+    public RadioButton rdbHabitus_12;
+    public RadioButton rdbHabitus_13;
+    public RadioButton rdbHabitus_14;
+    public RadioButton rdbHabitus_15;
+    public CheckBox chkBloeiwijze;
+    public RadioButton rdbBloeiwijze_Aar;
+    public RadioButton rdbBloeiwijze_BredePluim;
+    public RadioButton rdbBloeiwijze_Etage;
+    public RadioButton rdbBloeiwijze_BolofKnop;
+    public RadioButton rdbBloeiwijze_Margrietachtig;
+    public RadioButton rdbBloeiwijze_Scherm;
+    public RadioButton rdbBloeiwijze_SmallePluim;
+    public CheckBox chkLevensvormVolgensRaunkhiaer;
+    public RadioButton rdbLevensvorm_1;
+    public RadioButton rdbLevensvorm_2;
+    public RadioButton rdbLevensvorm_3;
+    public RadioButton rdbLevensvorm_4;
+    public RadioButton rdbLevensvorm_5;
+    public RadioButton rdbLevensvorm_6;
+    public RadioButton rdbLevensvorm_7;
+    public RadioButton rdbLevensvorm_8;
+    public RadioButton rdbLevensvorm_9;
+    public ComboBox cboOntwikkelingssnelheid;
     private InfoTables infoTables;
     private Connection dbConnection;
 
     public void initialize() throws SQLException {
         dbConnection = Database.getInstance().getConnection();
-
-        /*instellen exit, minimize en resize button*/
-        //ScreenControl();
-
         /*infotabel object aanmaken*/
         InfoTablesDAO infotablesDAO = new InfoTablesDAO(dbConnection);
-       // infoTables = infotablesDAO.getInfoTables();
-
-        /*comboboxes vullen*/
-       // FillComboboxes(infoTables);
+       infoTables = infotablesDAO.getInfoTables();
 
 
     }
-    /**
-     @param infotables -> lijst van alle lijsten van gegevens uit de naakte tabellen
-     @author bradley
-     Functie om comboboxes te vullen met alle gegevens uit de database
-     */
-    public void FillComboboxes(InfoTables infotables){
-        //type
-         System.out.println(infotables.getTypes().toString());
-        cboType.getItems().addAll(infotables.getTypes());
-        //familie
-        cboFamilie.getItems().addAll(infotables.getFamilies());
-        //bladgrootte
-        cboBladgrootte.getItems().addAll(infotables.getBladgroottes());
-        //bladvorm
-        cboBladvorm.getItems().addAll(infotables.getBladvormen());
-        //Levensvorm
 
-        //BehandelingMaand
-        cboMaand.getItems().addAll("Januari", "februari", "maart", "april", "mei", "juni", "juli","augustus","september", "oktober", "november", "december");
-        //ratio
-        cboRatio.getItems().addAll(infotables.getBloeiBladRatios());
-        //spruitfenologie
-        cboSpruitFenologie.getItems().addAll(infotables.getSpruitfenologieen());
-        //reactie antagonistische omgeving
-        cboReactie.getItems().addAll(infotables.getAntagonistischeOmgevingsReacties());
-        //behandeling
-
-    }
 
     public void Clicked_Search(MouseEvent mouseEvent) {
 
     }
-
-
-    /**
-     @author bradley
-     Functie om scherm aan te passen tussen volledig scherm en normale grootte
-     */
-    public void resize(){
-        if(maximized == true){
-            btnMaximize.setOnAction(e ->
-                    ( (Stage) ( (Button) e.getSource() ).getScene().getWindow() ).setMaximized(false));
-            maximized=false;
-        } else {
-            btnMaximize.setOnAction(e ->
-                    ( (Stage) ( (Button) e.getSource() ).getScene().getWindow() ).setMaximized(true));
-            maximized=true;
-        }
-    }
-
-    /**
-     @author bradley
-     Functie om scherm beheer knoppen te gebruiken
-     */
-   /* public void ScreenControl(){
-        btnExit.setOnAction(e ->
-                ((Stage) ((Button) e.getSource()).getScene().getWindow()).close()
-        );
-
-        btnminimize.setOnAction(e ->
-                ((Stage) ((Button) e.getSource()).getScene().getWindow()).setIconified(true)
-        );
-        resize();
-    }
-
-    /**
-     @author bradley
-     wanneer hier op geklikt wordt, wordt de schermgrootte aangepast
-     */
-    /*public void Clicked_Resize(MouseEvent mouseEvent) {
-        resize();
-    }*/
 
     /**
      * @author Bradley Velghe
@@ -198,7 +178,7 @@ public class Controller {
      * @param slider
      * @return true > slider is beweegbaar || false > slider is niet beweegbaar
      */
-  /*  public boolean Togledisable(CheckBox box, Slider slider){
+   public boolean Togledisable(CheckBox box, Slider slider){
         if(box.isSelected()){
             slider.setDisable(false);
             return true;
@@ -206,31 +186,8 @@ public class Controller {
             slider.setDisable(true);
             return false;
         }
-    }*/
-/*
-    public void Clicked_Bezonning(MouseEvent mouseEvent) {
-        Togledisable(chkBezonning, slrBezonning);
     }
 
-    public void Clicked_Vochtbehoefte(MouseEvent mouseEvent) {
-        Togledisable(chkVochtbehoefte, slrVochtbehoefte);
-    }
-
-    public void Clicked_VoedingsBehoefte(MouseEvent mouseEvent) {
-        Togledisable(chkVoedingsbehoefte, slrVoedingsbehoefte);
-    }
-
-    public void Clicked_Ontwikkelingssnelheid(MouseEvent mouseEvent) {
-        Togledisable(chkOntwikkelingsnelheid, slrOntwikkelingssnelheid);
-    }
-
-    public void Clicked_Pollenwaarde(MouseEvent mouseEvent) {
-        Togledisable(chkPollenwaarde, slrPollenwaarde);
-    }
-
-    public void Clicked_Nectarwaarde(MouseEvent mouseEvent) {
-        Togledisable(chkNectarwaarde, slrNectarwaarde);
-    }*/
 }
 
 
--- src/plantenApp/java/model/PropertiesMaps.java
+++ src/plantenApp/java/model/PropertiesMaps.java
@@ -1,7 +1,46 @@
 package plantenApp.java.model;
 
+import javafx.beans.property.StringProperty;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 public class PropertiesMaps  {
-    enum ObjectNames {
-        
+    enum EintBindings {
+        VOEDINGSBEHOEFTE,
+        SOCIABILITEIT,
+        NECTARWAARDE,
+        POLLENWAARDE,
+
+
     }
+
+    enum EstringBindings {
+        REACTIEANTAGONISTISCHEOMGEVING,
+        ONTWIKKELINGSSNELHEID,
+        LEVENSDUUR,
+        STRATEGIE,
+        BIJVRIENDELIJK,
+        VLINDERVRIENDELIJK,
+        EETBAAR,
+
+    }
+
+    enum EintArrayBindings {
+
+    }
+
+    enum EstringArrayBindings {
+
+    }
+
+    EnumMap<EintBindings, IntProperty> intProperties;
+    EnumMap<EintBindings, StringProperty> stringProperties;
+
+    public PropertiesMaps() {
+        intProperties = new EnumMap<EintBindings, IntProperty>(EintBindings.class);
+
+        intProperties.put(EintBindings.VOEDINGSBEHOEFTE, new IntProperty());
+    }
+
 }
