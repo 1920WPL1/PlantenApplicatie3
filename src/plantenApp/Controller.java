610056234cb3f3952d4a3fcc39ff5d4fd639e73d
Angelo Petyt <angelo.petyt@student.vives.be>
2020-06-11 17:43:23 +0200

    CSS*

--- src/plantenApp/Controller.java
+++ src/plantenApp/Controller.java
@@ -722,42 +722,6 @@ public class Controller {
         window.show();
 
          */
-
-        /*
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
-        );
-        File selectedFile = fileChooser.showOpenDialog(lblBezonning.getScene().getWindow());
-        System.out.println(selectedFile);
-
-        byte[] blob = convertFileContentToBlob(selectedFile.getPath());
-        FotoDAO fotoDAO = new FotoDAO(dbConnection);
-        fotoDAO.InsertFoto("smalle pluim", blob);
-         */
-    }
-
-    public static byte[] convertFileContentToBlob(String filePath) throws IOException {
-        // create file object
-        File file = new File(filePath);
-        // initialize a byte array of size of the file
-        byte[] fileContent = new byte[(int) file.length()];
-        FileInputStream inputStream = null;
-        try {
-            // create an input stream pointing to the file
-            inputStream = new FileInputStream(file);
-            // read the contents of file into byte array
-            inputStream.read(fileContent);
-        } catch (IOException e) {
-            throw new IOException("Unable to convert file to byte array. " +
-                    e.getMessage());
-        } finally {
-            // close input stream
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        }
-        return fileContent;
     }
 }
 
